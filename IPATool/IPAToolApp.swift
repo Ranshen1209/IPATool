7dc40b08c6c1d39b3c244cbd2ecce8f9720d02d4
Jones <153377475+Ranshen1209@users.noreply.github.com>
2026-03-31 01:17:24 +0800

    Initial Commit

---
+++ IPATool/IPAToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPAToolApp.swift
+//  IPATool
+//
+//  Created by Ariel on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct IPAToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
